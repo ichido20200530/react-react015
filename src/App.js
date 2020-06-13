86ca711aaf9a06c129352cb2db040e03e220b2dd
ichido <ichido@live.jp>
2020-06-13 19:25:53 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Appコンポーネント
+class App extends Component {
+
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render () {
+    return (
+      <div>
+        <h1>Redux</h1>
+        <Message />
+        <Button />
+      </div>
+    );
+  }
 }
+
+//ストアのコネクト
+App = connect () (App);
+
+//メッセージ表示のコンポーネント
+class Message extends Component {
+  style = {
+    fontSize: "20pt",
+    padding: "20px 5px",
+  }
+
+  render () {
+    return (
+      <p style={this.style}>
+        {this.props.Message}: {this.props.counter}
+      </p>
+    );
+  }
+}
+
+//ストアのコネクト
+Message = connect( ( state ) => state) ( Message );
+
+//ボタンのコンポーネント
+class Button extends Component {
+  style = {
+    fontSize: "16pt",
+    padding: "5px 10px",
+  }
+
+  constructor ( props ) {
+    super( props );
+    this.doAction = this.doAction.bind(this);
+  }
+
+  //ボタンクリックでディスパッチを実行
+  doAction ( e ) {
+    if ( e.shiftKey ) {
+      this.props.dispatch({ type: 'DECREMENT' });
+    } else if ( e.ctrlKey ) {
+      this.props.dispatch({ type: 'RESET' });
+    } else {
+      this.props.dispatch({ type: 'INCREMENT'})
+    }
+  }
+
+  render () {
+    return (
+      <button style={this.style} onClick={this.doAction}>Click</button>
+    );
+  }
+}
+
+//ストアのコネクト
+Button = connect () ( Button );
 
 export default App;
